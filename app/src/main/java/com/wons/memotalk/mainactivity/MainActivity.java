6dff87c687fe6bccf8bcb42fe20acde8e9b6e33a
wonho96 <wono96@naver.com>
2024-12-14 23:20:02 +0900

    midpotin

--- app/src/main/java/com/wons/memotalk/mainactivity/MainActivity.java
+++ app/src/main/java/com/wons/memotalk/mainactivity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.wons.memotalk.mainactivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,6 +16,7 @@ import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.wons.memotalk.R;
 import com.wons.memotalk.databinding.ActivityMainBinding;
+import com.wons.memotalk.databinding.DialogAddTitleBinding;
 import com.wons.memotalk.entity.Tab;
 import com.wons.memotalk.mainactivity.adapter.DemoCollectionAdapter;
 import com.wons.memotalk.mainactivity.viewmodels.ListViewModel;
@@ -57,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+        setTabUtilsOnCLick();
     }
 
     private void setViewPager() {
@@ -69,6 +74,55 @@ public class MainActivity extends AppCompatActivity {
         new TabLayoutMediator(binding.tab, binding.pager,
                 (tab, position) -> tab.setText(tabViewModel.getTabs().getValue().get(position).title)
         ).attach();
+    }
+
+    private void setTabUtilsOnCLick() {
+        binding.btnAddTab.setOnClickListener((view) -> {
+            showPopupMenu();
+        });
+    }
+
+    private void showPopupMenu() {
+        // PopupMenu 객체 생성
+        PopupMenu popupMenu = new PopupMenu(this, binding.btnMenu);
+        popupMenu.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu()); // 메뉴 리소스 설정
+
+        // 메뉴 항목 클릭 리스너 설정
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.btn_addTab) {
+
+                }
+
+                if (menuItem.getItemId() == R.id.btn_d_list) {
+
+                }
+
+                if (menuItem.getItemId() == R.id.btn_c_tab_name) {
+
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
+
+    private void tabRename() {
+
+    }
+
+    private void addTab() {
+        AlertDialog dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        dialog = builder.create();
+
+        DialogAddTitleBinding binding1 = DialogAddTitleBinding.inflate(getLayoutInflater());
+
+    }
+
+    private void delTab() {
+        //todo 보류 -> 현재 탭의 메모룸 메모장 다 삭제 해야됨
     }
 
 }
